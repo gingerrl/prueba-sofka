3dc73d9274d011b658cc0a8eec0fcc4e17c01832
unknown <ginger1881@hotmail.com>
2025-03-07 18:11:36 -0500

    fix: missing validations added

--- src/app/pages/form-page/form-page.component.ts
+++ src/app/pages/form-page/form-page.component.ts
@@ -1,8 +1,8 @@
+import { formatDate } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { MetaData, NgEventBus } from 'ng-event-bus';
-import { formatDate } from '@angular/common';
 import { ListService } from '../../services/list-service.service';
 
 @Component({
@@ -42,53 +42,65 @@ export class FormPageComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.eventBus.on('edit').subscribe((meta:MetaData) => {
-      // console.log("dataaa",meta)
-        this.onListChanges(meta.data)
+    this.eventBus.on('edit').subscribe((meta: MetaData) => {
+      this.isCreate = false;
+      this.onProductChanges(meta.data);
     });
   }
 
-
-  onButtonBack(){
-    this.router.navigate(['/home'])
-  }
-
   onButtonSend() {
-    this.listService.verificationProduct(this.form.controls['id'].value).subscribe((data) =>{
-      this.onListAdd(data)
-    })
+    if (!this.isCreate) return this.onListUpdate();
+    this.listService
+      .verificationProduct(this.form.controls['id'].value)
+      .subscribe((data) => {
+        this.onListAdd(data);
+      });
   }
 
   onListAdd(exist: boolean) {
     if (!exist) {
       this.listService.addProduct(this.form.value).subscribe(() => {
         this.router.navigate([`/home`]);
-      })
+      });
     } else {
-      alert('El id ya existe')
+      alert('El id ya existe');
     }
   }
 
   onListUpdate() {
-    this.listService.updateProduct(this.form.value).subscribe(() => {
-      this.router.navigate([`/home`]);
-
-    })
+    this.listService
+      .updateProduct(this.form.controls['id'].value, this.form.value)
+      .subscribe(() => {
+        this.router.navigate([`/home`]);
+      });
   }
 
-
-
-
-  onListChanges(item:any) {
-    console.log("itemmm",item)
+  onProductChanges(item: any) {
     this.form.setValue({
       id: item.id,
       name: item.name,
       description: item.description,
       logo: item.logo,
       date_release: formatDate(item.date_release, 'YYYY-MM-dd', 'en', 'UTC'),
-      date_revision: formatDate(item.date_revision, 'YYYY-MM-dd', 'en', 'UTC')
-    })
+      date_revision: formatDate(item.date_revision, 'YYYY-MM-dd', 'en', 'UTC'),
+    });
   }
 
+  onButtonReseet() {
+    if (!this.isCreate) {
+      this.form.patchValue({
+        name: '',
+        description: '',
+        logo: '',
+        date_release: '',
+        date_revision: '',
+      });
+    } else {
+      this.form.reset('');
+    }
+  }
+
+  onButtonBack() {
+    this.router.navigate(['/home']);
+  }
 }
