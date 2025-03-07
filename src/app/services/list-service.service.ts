0d77b7c70da1ba507ebd7de136a2483710bcd297
unknown <ginger1881@hotmail.com>
2025-03-07 18:12:26 -0500

    fix: missing services added

--- src/app/services/list-service.service.ts
+++ src/app/services/list-service.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ProductInterface, Response } from '../interface/product-interface';
 import { map, Observable } from 'rxjs';
 import { Endpoints } from '../config/endpoints.enum';
+import { ProductInterface, Response } from '../interface/product-interface';
 
 @Injectable({
   providedIn: 'root',
@@ -15,23 +15,26 @@ export class ListService {
       .get<Response<ProductInterface[]>>(`${Endpoints.URL}products`)
       .pipe(
         map((resp) => {
-          console.log('mapp', resp);
           return resp.data;
         })
       );
   }
 
   addProduct(body: ProductInterface[]) {
-    console.log('addd', body);
     return this.http.post(`${Endpoints.URL}products`, body);
   }
 
-  updateProduct(body: ProductInterface[]) {
-    return this.http.put(`${Endpoints.URL}`, body);
+  updateProduct(id: string, body: ProductInterface[]) {
+    return this.http.put(`${Endpoints.URL}products/${id}`, body);
   }
+
   verificationProduct(id: string) {
     return this.http.get<boolean>(
       `${Endpoints.URL}products/verification/${id}`
     );
+  }
+
+  deleteProduct(id: string) {
+    return this.http.delete(`${Endpoints.URL}products/${id}`);
   }
 }
